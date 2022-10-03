fec09741d92e32195ec9f651bb533d13a8f336b8
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-03 17:43:48 +0800

    changes name of component

--- app/gallery-dashboard/components/Albums.tsx
+++ app/gallery-dashboard/components/Albums.tsx
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import { Link, Flex } from '@chakra-ui/react'
-import { PhotoCard } from '../../../components'
+import { AlbumCard } from 'components'
 
 export const Albums: React.FC = () => {
   return (
@@ -8,7 +8,7 @@ export const Albums: React.FC = () => {
       {Array.from({ length: 20 }).map((_arr, i) => (
         <NextLink href={`/album/${i}`} key={i} passHref>
           <Link textDecoration='none' _hover={{}}>
-            <PhotoCard key={i} />
+            <AlbumCard key={i} />
           </Link>
         </NextLink>
       ))}
--- components/AlbumCard.tsx
+++ components/AlbumCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Image, Stack, Text } from '@chakra-ui/react'
 
-export const PhotoCard: React.FC = () => {
+export const AlbumCard: React.FC = () => {
   return (
     <Box bgColor='#F5F5F5' py='1.125rem' px='0.875rem' borderRadius='lg'>
       <Image alt='photo-card' h='150px' w='150px' borderRadius='lg' />
