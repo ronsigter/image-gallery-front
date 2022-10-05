cbb4c9883675a3a191be2c28efa4fce4ca2159ae
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-05 13:23:06 +0800

    creates API endpoint for file upload

---
+++ pages/api/upload_photo.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { s3 } from 'lib/awsS3'
+
+type Data = {
+  message?: string | unknown
+  url?: string
+}
+
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  try {
+    let { name, type } = req.body
+
+    const fileParams = {
+      Bucket: AWS_BUCKET_NAME,
+      Key: name,
+      expires: 600,
+      ContentType: type,
+      ACL: 'public-read',
+    }
+
+    const url = await s3.getSignedUrlPromise('putObject', fileParams)
+
+    res.status(200).json({ url })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ message: error })
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+}
