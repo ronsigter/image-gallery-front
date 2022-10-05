be9ff705240ff31a123bc0dd1f6d1d5ffff84dfc
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-05 12:12:44 +0800

    fixes photocard viewer

--- components/ViewPhotoModal.tsx
+++ components/ViewPhotoModal.tsx
@@ -9,6 +9,8 @@ import {
   Button,
   Box,
   ModalProps,
+  Image,
+  Center,
 } from '@chakra-ui/react'
 import type { Photo } from 'graphql/gql'
 
@@ -19,13 +21,29 @@ interface ViewPhotoModalProps extends Omit<ModalProps, 'children'> {
 export const ViewPhotoModal: React.FC<ViewPhotoModalProps> = (props) => {
   const { photo = null, ...rest } = props
   return (
-    <Modal {...rest}>
+    <Modal isCentered {...rest}>
       <ModalOverlay />
       <ModalContent maxW='3xl'>
         <ModalHeader>{photo?.name || '---'}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Box w='100%' h='590px' bgColor='#D9D9D9' />
+          <Center
+            w='100%'
+            h={{ base: '70vw', md: '500px' }}
+            bgColor='#D9D9D9'
+            borderRadius='xl'
+            overflow='hidden'
+            cursor='pointer'
+          >
+            <Image
+              alt='photo-card'
+              bgColor='#D3D3D3'
+              borderRadius='lg'
+              overflow='hidden'
+              objectFit='cover'
+              fallbackSrc='/no-image.png'
+            />
+          </Center>
         </ModalBody>
         <ModalFooter>
           <Button mr={3} onClick={rest.onClose}>
